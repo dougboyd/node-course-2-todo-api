46d80dc07af0ea559132f66131a8228a9459b907
dougboyd <doug@dougboyd.com.au>
2018-06-22 09:30:30 +1000

    Refactor and addition of create todo route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 const port = process.env.PORT || 3000;
 
@@ -16,7 +22,6 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-
   todo.save().then((doc) => {
     res.status(200);
     res.send(`saved documet: ${doc}`);
